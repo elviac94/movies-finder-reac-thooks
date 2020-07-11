980becc914b31c793dd4541d84297afc9b635f36
Elvira <elvi_ac94@hotmail.com>
2020-07-12 00:26:31 +0200

    add home styles with material.ui

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,10 @@
 import React, { useState} from 'react';
 import { Container, Typography, Card, Grid, TextField, Button } from '@material-ui/core';
+import styles from './style';
 
 export default () =>{
     const [searchText, setSearchText] = useState('');
+    const classes = styles()
     const handleSearchTextChange = event =>{
         setSearchText(event.target.value)
     };
@@ -16,11 +18,11 @@ export default () =>{
     };
     
     return(
-        <Container>
-            <Card>
-                <Grid container>
+        <Container className={classes.container}>
+            <Card className={classes.cardContainer}>
+                <Grid container className={classes.titleGridContainer}>
                     <Grid>
-                        <Typography>Bienvenido!</Typography>
+                        <Typography className={classes.title}>Bienvenido!</Typography>
                     </Grid>
                     <Grid>
                         <label>Icono</label>
@@ -29,11 +31,12 @@ export default () =>{
                 <TextField
                 value={searchText}
                 placeholder="Buscar..."
+                className={classes.textFieldSearch}
                 onChange={handleSearchTextChange}
                 />
-                <Grid>
+                <Grid className={classes.buttonsContainer}>
                     <Button variant="contained" onClick={handleCleanTextClick}>Limpiar</Button>
-                    <Button variant="contained" color="primary" size="large" onClick={handleSearchTextClick}>Buscar</Button>
+                    <Button variant="contained" className={classes.searchButton} color="primary" size="large" onClick={handleSearchTextClick}>Buscar</Button>
                 </Grid>
 
             </Card>
